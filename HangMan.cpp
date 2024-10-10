d09ec96c1f63c105a41ffb5113dece7483846f32
LydiaFrame <156206633+LydiaFrame@users.noreply.github.com>
2024-10-10 22:03:31 +0000

    Not finished test

---
+++ HangMan.cpp
@@ -0,0 +1,25 @@
+/*
+    Names: Lydia Frame, Darrell Holbrook
+    Date: 10/10/24
+    Program: Hangman 
+    Email: lydia.r.frame@gmail.com
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num;
+
+    num = 2;
+
+    cout << num; 
+
+
+    return 0;
+
+}
+
+// Class for Hangman figure
